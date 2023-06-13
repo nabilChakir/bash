1b96307b0d01ddd906705dec1a4d7e7c5127e3bf
nabil chakir <nach212@icloud.com>
2023-06-13 22:29:03 +0200

    first commit

--- friend.sh
+++ friend.sh
@@ -10,10 +10,10 @@ mode=$1     # argument obligatoire du script, doit être égal à "interactif" o
 lines_number=$(cat "/Users/nabilchakir/BeCode/Bash/blagues.txt" | wc -l)
 
 afficher_blague(){
-	line=$(jot -r 1 1 $lines_number)
-	blague=$(cat "/Users/nabilchakir/BeCode/Bash/blagues.txt" | head -n "$line" | tail -n 1)
-	echo $blague
-	say $blague
+    line=$(jot -r 1 1 $lines_number)
+    blague=$(cat "/Users/nabilchakir/BeCode/Bash/blagues.txt" | head -n "$line" | tail -n 1)
+    echo $blague
+    say $blague
 }
 
 afficher_heure(){
@@ -26,57 +26,57 @@ afficher_calcul(){
     resultat=$(echo "scale=2; $1" | bc)
     calcul=$(echo "Le résultat de votre calcul est égal à $resultat")
     echo $calcul
-	resultat_audio="Le résultat de votre calcul est égal à $(echo $resultat | sed 's/\./ virgule /')"
+    resultat_audio="Le résultat de votre calcul est égal à $(echo $resultat | sed 's/\./ virgule /')"
     say "$resultat_audio"
 }
 
 afficher_meteo(){
     echo "Prévisions météorologiques:"
-	say "Voici ci-dessous les prévisions météo pour les 3 prochains jours"
-	curl wttr.in
+    say "Voici ci-dessous les prévisions météo pour les 3 prochains jours"
+    curl wttr.in
 }
 
 if [ $# -eq 0 ]; then
-	echo -e "Le script prend soit 2 arguments, \"non-interactif\" et \"une opération mathématique\", soit un unique argument égal à \"interactif\""
-	echo "Veuillez relancer le script avec le(s) bon(s) argument(s)"
+    echo -e "Le script prend soit 2 arguments, \"non-interactif\" et \"une opération mathématique\", soit un unique argument égal à \"interactif\""
+    echo "Veuillez relancer le script avec le(s) bon(s) argument(s)"
 
 # Si le premier argument est égale à "non-interactif", l'opération mathématique à effectuer est fournie en tant que second argument
 elif [ "$mode" =  "non-interactif" ]; then
-	afficher_blague
-	afficher_heure
+    afficher_blague
+    afficher_heure
     afficher_calcul $2
     afficher_meteo
 
 elif [ "$mode" = "interactif" ]; then
     # -n permet d'activer l'interprétation des caractères d'échappement 
-	echo -e "\nPour QUITTER le shell interactif, faites ctrl+C\n\nVoici les phrases prédéfinies auxquelles cet assistant peut répondre:"
-	echo -e "Racontes moi une blague\nQuelle heure est-il?\nCalcules moi ...\nRenseignes moi sur la météo\n"
-	MY_PROMPT='Veuillez entrer une question$ '
-	while :
-	do	
-  		# -n empeche l'affichage du saut de ligne (\n) à la fin de la sortie 
-		echo -n "$MY_PROMPT"
-  		# lecture de l'entrée utilisateur à partir de la CLI
-		read line
+    echo -e "\nPour QUITTER le shell interactif, faites ctrl+C\n\nVoici les phrases prédéfinies auxquelles cet assistant peut répondre:"
+    echo -e "Racontes moi une blague\nQuelle heure est-il?\nCalcules moi ...\nRenseignes moi sur la météo\n"
+    MY_PROMPT='Veuillez entrer une question$ '
+    while :
+    do	
+  	# -n empeche l'affichage du saut de ligne (\n) à la fin de la sortie 
+	echo -n "$MY_PROMPT"
+  	# lecture de l'entrée utilisateur à partir de la CLI
+	read line
 		
-		if [ "$line" = "Racontes moi une blague" ]; then
-        	afficher_blague
-  		elif [ "$line" = "Quelle heure est-il?" ]; then 
-			afficher_heure
-		elif [[ $line =~ [Cc]alcules\ (moi)? ]]; then
-			if [[ $line =~ [\-]?[0-9]+[\+\-\*\/\^][0-9]+([\+\-\*\/\^][0-9]+)* ]]; then
-				calcul="${BASH_REMATCH[0]}"
-				afficher_calcul $calcul
-			else
-				echo "Il n'y a aucune opération à calculer ou l'opération demandée n'est pas pris en charge"
-			fi
-		elif [ "$line" = "Renseignes moi sur la météo" ]; then
+	if [ "$line" = "Racontes moi une blague" ]; then
+            afficher_blague
+  	elif [ "$line" = "Quelle heure est-il?" ]; then 
+	    afficher_heure
+	elif [[ $line =~ [Cc]alcules?\ (moi)? ]]; then
+	    if [[ $line =~ [\-]?[0-9]+[\+\-\*\/\^][0-9]+([\+\-\*\/\^][0-9]+)* ]]; then
+	        calcul="${BASH_REMATCH[0]}"
+		afficher_calcul $calcul
+	    else
+		echo "Il n'y a aucune opération à calculer ou l'opération demandée n'est pas pris en charge"
+	    fi
+	elif [ "$line" = "Renseignes moi sur la météo" ]; then
             afficher_meteo
-		else
-			echo "Je n'ai pas compris ce que vous voulez me demander, veuillez reformuler votre question ou appuye ctrl+C pour quitter"
-		fi
-	done
-	exit 0  # spécifie explicitement que le script a terminé son exécution avec succès
+	else
+	    echo "Je n'ai pas compris ce que vous voulez me demander, veuillez reformuler votre question ou appuye ctrl+C pour quitter"
+	fi
+    done
+    exit 0  # spécifie explicitement que le script a terminé son exécution avec succès
 
 fi
 
